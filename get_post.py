e840ab901349ba10e794c22d4087d160064a0dbc
Kewbish <kewbish@gmail.com>
2020-07-23 19:42:19 -0700

    Add HTML styling

--- get_post.py
+++ get_post.py
@@ -1,7 +1,10 @@
 from feedparser import parse
 
 feed = parse("https://kewbish.github.io/blog/index.xml").entries
-latest = f"[{feed[0].title}]({feed[0].link})\n{feed[0].published} - {feed[0].description}"
+latest = f"""
+<div style="border: 1px solid; border-radius: 5px; padding: 0.5% 1%; width: 50%;">
+\n<h3><a href="{feed[0].link}">{feed[0].title} 🖋️</a></h3>\n
+<p>{feed[0].description} - <small>{feed[0].published}</small></p>\n</div>"""
 
 farr = []
 with open("README.md", "r", encoding='utf8') as x:
